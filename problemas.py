b17e5cfc0e1e443f874e9a0a76845df80c8dd103
Ricardo050427 <hernandezastorgafrancisco@gmail.com>
2026-02-28 07:20:13 -0700

    correcciones

--- problemas.py
+++ problemas.py
@@ -31,16 +31,16 @@ class PbCamionMagico(busquedas.ProblemaBusqueda):
     ----------------------------------------------------------------------------------
     
     """
-    def __init__(self, meta):
+    def __init__(self):
         pass
 
     def acciones(self, estado):
         posicion, meta = estado
         acciones = []
-        if estado <= self.meta:
+        if posicion <= meta:
             acciones.append("un paso")
 
-        if estado * 2 <= self.meta:
+        if posicion * 2 <= meta:
             acciones.append("camion magico")
 
         return acciones
@@ -76,8 +76,35 @@ class PbCamionMagico(busquedas.ProblemaBusqueda):
 
 def h_1_camion_magico(nodo):
     """
-    DOCUMENTA LA HEURÍSTICA QUE DESARROLLES Y DA UNA JUSTIFICACIÓN
-    PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
+    Esta heuristica asume un escenario extremadamente optimista, lo que seria un
+    problema relajado, simplemente calcula cuantas veces tendriamos que usar el camion magico
+    para alcanzar o rebasar la meta, pero imaginando que el camión cuesta solo 1 minuto
+    en lugar de 2. Es admisible porque al aplicar este descuento, garantizamos
+    matematicamente que la estimacion siempre sera menor o igual al tiempo real
+    que tomaria llegar. Es decir, nunca va a sobrestimar el costo.
+
+    """
+    estado_actual, meta = nodo.estado
+    saltos = 0
+
+    while estado_actual < meta:
+        estado_actual *= 2
+        saltos += 1
+
+    return saltos
+
+# ------------------------------------------------------------
+#  Desarrolla otra política admisible.
+#  Analiza y di porque piensas que es (o no es) dominante una
+#  respecto otra política
+# ------------------------------------------------------------
+
+def h_2_camion_magico(nodo):
+    """
+    Como esta heuristica trabaja desde la meta hasta el estado inicial y no
+    al reves, no es cuestion de "atinarle" a que movimiento hacer, en cualquier
+    momento sabe si usar camion magico (si divisible entre 2) o solo dar
+    un paso (cuando no es divisible entre 2).
 
     """
     estado_actual, meta = nodo.estado
@@ -93,20 +120,6 @@ def h_1_camion_magico(nodo):
             costo_estimado += 1
 
     return costo_estimado
-
-# ------------------------------------------------------------
-#  Desarrolla otra política admisible.
-#  Analiza y di porque piensas que es (o no es) dominante una
-#  respecto otra política
-# ------------------------------------------------------------
-
-def h_2_camion_magico(nodo):
-    """
-    DOCUMENTA LA HEURÍSTICA DE DESARROLLES Y DA UNA JUSTIFICACIÓN
-    PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
-
-    """
-    return 0
 
 # ------------------------------------------------------------
 #  Desarrolla el modelo del cubo de Rubik
@@ -179,18 +192,18 @@ def compara_metodos(problema, pos_inicial, heuristica_1, heuristica_2):
     @param heuristica_2: Una función de heurística
 
     """
-    solucion1 = busquedas.busqueda_A_estrella(problema, heuristica_1, pos_inicial)
-    solucion2 = busquedas.busqueda_A_estrella(problema, heuristica_2, pos_inicial)
+    solucion1, nodo1 = busquedas.busqueda_A_estrella(problema, pos_inicial, heuristica_1)
+    solucion2, nodo2 = busquedas.busqueda_A_estrella(problema, pos_inicial, heuristica_2)
     
     print('-' * 50)
     print('Método'.center(12) + 'Costo'.center(18) + 'Nodos visitados'.center(20))
     print('-' * 50 + '\n')
     print('A* con h1'.center(12) 
           + str(solucion1.costo).center(18) 
-          + str(solucion1.nodos_visitados))
+          + str(nodo1))
     print('A* con h2'.center(12) 
           + str(solucion2.costo).center(20) 
-          + str(solucion2.nodos_visitados))
+          + str(nodo2))
     print('-' * 50 + '\n')
 
 
@@ -198,13 +211,15 @@ if __name__ == "__main__":
 
     # Compara los métodos de búsqueda para el problema del camión mágico
     # con las heurísticas que desarrollaste
-    pos_inicial = XXXXXXXXXX  # <--- PONLE LA POSICIÓN INICIAL QUE QUIERAS
-    problema = PbCamionMagico( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
+    print("\n" + "=" * 50)
+    print("=== RESOLVIENDO EL CAMION MAGICO ===")
+    pos_inicial = (1, 50)  # <--- PONLE LA POSICIÓN INICIAL QUE QUIERAS
+    problema = PbCamionMagico()  # <--- PONLE LOS PARÁMETROS QUE NECESITES
     compara_metodos(problema, pos_inicial, h_1_camion_magico, h_2_camion_magico)
     
     # Compara los métodos de búsqueda para el problema del cubo de rubik
     # con las heurísticas que desarrollaste
-    pos_inicial = XXXXXXXXXX  # <--- PONLE LA POSICIÓN INICIAL QUE QUIERAS
-    problema = PbCuboRubik( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
-    compara_metodos(problema, h_1_problema_1, h_2_problema_1)
+    #pos_inicial = XXXXXXXXXX  # <--- PONLE LA POSICIÓN INICIAL QUE QUIERAS
+    #problema = PbCuboRubik( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
+    #compara_metodos(problema, h_1_problema_1, h_2_problema_1)
     
